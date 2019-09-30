b385f5e32b554d1ab885ced8b61368c3dfc70070
George Pittarelli <ineedthis@gjp.cc>
2019-09-29 21:16:25 -0700

    Add prettier + run it on src/

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import {createService, ServiceName} from 'ineedthis';
-import killable, {KillableServer} from 'killable';
+import { createService, ServiceName } from 'ineedthis';
+import killable, { KillableServer } from 'killable';
 import express from 'express';
 
 type ExpressServiceOptions = {
   setup: (server: any, deps: any) => never;
-  port?: (deps: any) => number;
+  port?: (deps: any) => (number | string) | number;
   dependencies: ServiceName[];
 };
 
@@ -15,17 +15,23 @@ function getPort(opts: ExpressServiceOptions, deps: any): number {
   return opts.port || 3000;
 }
 
-module.exports = function createExpressService(name: ServiceName, opts: ExpressServiceOptions) {
-  return createService<KillableServer, () => (deps: any) => Promise<KillableServer>>(name, {
+module.exports = function createExpressService(
+  name: ServiceName,
+  opts: ExpressServiceOptions
+) {
+  return createService<
+    KillableServer,
+    () => (deps: any) => Promise<KillableServer>
+  >(name, {
     dependencies: opts.dependencies || [],
 
-    start: function () {
-      return function (deps: any) {
+    start: function() {
+      return function(deps: any) {
         var app = express();
         opts.setup(app, deps);
 
-        return new Promise(function (resolve, reject) {
-          var server = app.listen(getPort(opts, deps), function () {
+        return new Promise(function(resolve, reject) {
+          var server = app.listen(getPort(opts, deps), function() {
             resolve(killable(server));
           });
           server.on('error', reject);
@@ -33,14 +39,14 @@ module.exports = function createExpressService(name: ServiceName, opts: ExpressS
       };
     },
 
-    stop: function (server: KillableServer) {
-      return new Promise(function (resolve) {
-        server.kill(function () {
+    stop: function(server: KillableServer) {
+      return new Promise(function(resolve) {
+        server.kill(function() {
           resolve();
         });
       });
     }
-  })
+  });
 };
 
 module.exports.__ineedthis__isCreateService = true;
--- src/types/killable.d.ts
+++ src/types/killable.d.ts
@@ -1,6 +1,8 @@
 declare module 'killable' {
   import { Server } from 'http';
 
-  export type KillableServer = Server & { kill: (callback: () => void) => never; };
+  export type KillableServer = Server & {
+    kill: (callback: () => void) => never;
+  };
   export default function killable(server: Server): KillableServer;
 }
